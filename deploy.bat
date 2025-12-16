fd23a019b4d216f3afc8cfb1e012cd09fa7cd90c
Jean Nguyen <community@thebeanfamily.org>
2025-12-17 05:51:03 +0700

    update sw.js

--- deploy.bat
+++ deploy.bat
@@ -4,7 +4,7 @@ ssh base_43  "service tomcat stop"
 echo "===== Remove MDM frontend folder ====="
 ssh base_43  "rm -rf /opt/tomcat/webapps/ROOT"
 echo "===== Deploy MDM service ====="
-scp .\base-tomcat\ROOT.xml base_43:/opt/tomcat/conf/Catalina/localhost/ROOT.xml
+@REM scp .\base-tomcat\ROOT.xml base_43:/opt/tomcat/conf/Catalina/localhost/ROOT.xml
 scp .\server\target\launcher.war base_43:/opt/tomcat/webapps/ROOT.war
 echo "===== Start TOMCAT server ====="
 ssh base_43 "service tomcat start && tail -f /opt/tomcat/logs/catalina.out "
