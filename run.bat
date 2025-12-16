23ce02a9d6d3cf25b3cda310506f3c1bf7fadf07
Jean Nguyen <community@thebeanfamily.org>
2025-12-17 03:48:01 +0700

    update

--- run.bat
+++
@@ -1,6 +0,0 @@
-copy /Y C:\Git\thebase\base_mdm_server\server\target\launcher.war C:\Git\thebase\base_mdm_server\tomcat_server\webapps\ROOT.war
-set CATALINA_HOME=C:\tools\tomcat9
-set CATALINA_BASE=C:\Git\thebase\base_mdm_server\tomcat_server
-cd base-mdm
-%CATALINA_HOME%\bin\catalina.bat run
-cd ..
